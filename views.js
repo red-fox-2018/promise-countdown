6d828d203771ec0efed718e8a95f4dd34cf4e2fe
annisaayu <annisaayu569@gmail.com>
2018-04-18 12:47:30 +0700

    promise countdown

--- views.js
+++ views.js
@@ -6,6 +6,18 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let menit = Math.floor(seconds/60).toString();
+  let detik = (seconds%60).toString();
+
+  if(menit.length < 2){
+    menit = `0${menit}`;
+  }
+  if(detik.length < 2){
+    detik = `0${detik}`;
+  }
+
+  console.log(chalk.cyan(figlet.textSync(`${menit} : ${detik}`)));
+
 };
 
 module.exports = { displayCountdown };
