ed74402a6ac25793309fae56abd844b930fa257a
cantdocpp <cihuyman95@gmail.com>
2018-04-18 15:17:12 +0700

    first commit

--- views.js
+++ views.js
@@ -5,7 +5,21 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  var menit = Math.floor(seconds / 60);
+  var detik = seconds % 60;
+  if (menit < 10) {
+    if (detik < 10) {
+      console.log(chalk.cyan(figlet.textSync(`0${menit}:0${detik}`)));
+    } else {
+      console.log(chalk.cyan(figlet.textSync(`0${menit}:${detik}`)));
+    }
+  } else {
+    if (detik < 10) {
+      console.log(chalk.cyan(figlet.textSync(`${menit}:0${detik}`)));
+    } else {
+      console.log(chalk.cyan(figlet.textSync(`${menit}:${detik}`)));
+    }
+  }
 };
 
 module.exports = { displayCountdown };
