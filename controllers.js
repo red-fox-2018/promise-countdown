f8502b1674d831eb75f243c58d10c5397ae550a0
A N Afifudin <33721254+davidjoshua87@users.noreply.github.com>
2018-04-18 14:15:40 +0700

    Edit controller.js

--- controllers.js
+++ controllers.js
@@ -10,17 +10,21 @@ const {
 } = require("./views");
 
 const timer = seconds =>
-   new Promise((resolve, reject) => {
-      let myTime = setInterval(() => {
+  new Promise((resolve, reject) => {
+    const now = Date.now();
+    const then = now + seconds * 1000;
 
-         displayCountdown(seconds);
-         seconds--;
-         if (seconds < 0) {
-            clearInterval(myTime);
-            resolve();
-         }
-      }, 1000);
-   });
+    displayCountdown(seconds - 1);
+    setInterval(() => {
+      const secondsLeft = Math.floor((then - Date.now()) / 1000);
+      if (secondsLeft < 0) {
+        clearInterval();
+        resolve();
+      } else {
+        displayCountdown(secondsLeft);
+      }
+    }, 1000);
+  });
 
 module.exports = {
    timer
